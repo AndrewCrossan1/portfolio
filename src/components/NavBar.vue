2679cf9685ea64f7f4e127cff2ba95061fd58fd3
AndrewCrossan1 <andrew.crossan23@outlook.com>
2022-12-12 20:24:55 +0000

    Added NavBar component with basic needs

---
+++ src/components/NavBar.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="navbar" class="sticky-top">
+        <b-container fluid class="bg-dark">
+            <b-container size="md">
+                <b-navbar toggleable="md" type="dark" variant="dark" class="p-3 sticky-top">
+                    <b-navbar-brand href="/">
+                        Andy's Portfolio
+                    </b-navbar-brand>
+                    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+                    <b-collapse id="nav-collapse" is-nav>
+                        <!--Right hand side of navbar-->
+                        <b-navbar-nav class="ms-auto">
+                            <b-nav-item>
+                                <b-link :to="{ name: 'home' }" :active="$route.name === 'home'" class="nav-link" tag="a">
+                                    <b-icon
+                                        icon="house-door-fill"
+                                    />
+                                    Home
+                                </b-link>
+                            </b-nav-item>
+                        </b-navbar-nav>
+                    </b-collapse>
+                </b-navbar>
+            </b-container>
+        </b-container>
+    </div>
+</template>
