bd414b36f9db532ac118c9b36fa34ba6512a83f2
AndrewCrossan1 <andrew.crossan23@outlook.com>
2022-12-12 20:26:26 +0000

    Configured Axios defaults and added BootstrapVue

--- src/main.js
+++ src/main.js
@@ -2,8 +2,26 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import BootstrapVue, {IconsPlugin} from 'bootstrap-vue'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+// Axios config
+Vue.prototype.$http = axios;
+Vue.prototype.$http.defaults.baseURL = process.env.VUE_APP_API_BASE_URL
+const token = localStorage.getItem('token');
+if (token) {
+  Vue.prototype.$http.defaults.headers.common['Authorization'] = token;
+}
 
 new Vue({
   router,
