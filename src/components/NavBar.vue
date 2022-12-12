553cb27135c8ba144d6cb1f95508ced66ae83ba6
AndrewCrossan1 <andrew.crossan23@outlook.com>
2022-12-12 20:54:41 +0000

    Changed styling of navbar to custom colours

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,9 +1,9 @@
 <template>
     <div id="navbar" class="sticky-top">
-        <b-container fluid class="bg-dark">
+        <b-container fluid class="bg-graphite-gray">
             <b-container size="md">
-                <b-navbar toggleable="md" type="dark" variant="dark" class="p-3 sticky-top">
-                    <b-navbar-brand href="/">
+                <b-navbar toggleable="md" class="p-2 sticky-top">
+                    <b-navbar-brand href="/" class="text-cornflower-blue">
                         Andy's Portfolio
                     </b-navbar-brand>
                     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
@@ -11,7 +11,7 @@
                         <!--Right hand side of navbar-->
                         <b-navbar-nav class="ms-auto">
                             <b-nav-item>
-                                <b-link :to="{ name: 'home' }" :active="$route.name === 'home'" class="nav-link" tag="a">
+                                <b-link :to="{ name: 'home' }" :active="$route.name === 'home'" class="nav-link text-cornflower-blue" tag="a">
                                     <b-icon
                                         icon="house-door-fill"
                                     />
