9d8947d00fc8a1850c304ae493debd14f8837531
AndrewCrossan1 <andrew.crossan23@outlook.com>
2022-12-12 20:25:16 +0000

    Added SEO to home route and page authentication

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import store from '@/store';
+import {seoGuardWithNext} from '@aminoeditor/vue-router-seo';
 
 Vue.use(VueRouter)
 
@@ -8,7 +10,19 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+    meta: {
+      requiresAuth: false,
+      seo: {
+        title: 'Home',
+        metaTags: [
+          {
+            name: 'description',
+            content: 'The home page of our example app.'
+          }
+        ]
+      }
+    }
   },
 ]
 
@@ -17,5 +31,25 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 })
+
+router.beforeEach(seoGuardWithNext)
+
+router.beforeEach((to, from, next) => {
+  if (to.name === 'login') {
+    if (store.getters.isLoggedIn) {
+      return next({name: 'home'})
+    } else {
+      next()
+    }
+  } else  if (to.meta && to.meta.requiresAuth === false) {
+    next()
+  } else {
+    if (store.getters.isLoggedIn) {
+      next()
+    } else {
+      next({name: 'login'})
+    }
+  }
+});
 
 export default router
