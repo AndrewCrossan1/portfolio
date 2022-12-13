a854638ec9905598b7c35420394e896476939b57
AndrewCrossan1 <andrew.crossan23@outlook.com>
2022-12-13 18:45:36 +0000

    Added first section to HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,33 @@
 <template>
-  <div class="home">
+  <div id="home">
+    <div id="section-1" class="vh-100 snap-1">
+      <b-container size="md">
+        <b-row>
+          <div class="col-lg-12 col-md-12 col-sm-12 col-xl-8 pt-5 text-align">
+            <p class="display-1 text-bebas-neue animate-1 animate__animated animate__fadeInLeft">
+              Hi, I'm <span class="text-cornflower-blue">Andrew</span>
+            <br/>
+            </p>
+            <p class="display-1 text-bebas-neue animate-2 animate__animated animate__fadeInLeft">
+              A <span class="text-cornflower-blue">Software Engineering</span> Student
+            <br/>
+            </p>
+            <p class="display-1 text-bebas-neue animate-3 animate__animated animate__fadeInLeft">
+              Welcome to my <span class="text-cornflower-blue">portfolio</span>
+            </p>
+          </div>
+          <div class="col-lg-12 col-md-12 col-sm-12 col-xl-4 py-5 my-auto animate-4 animate__animated animate__fadeIn">
+            <img src="../assets/images/undraw_programming_re_kg9v.svg" alt="Programmer" class="img-fluid"/>
+          </div>
+        </b-row>
+      </b-container>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path fill="#53565b" fill-opacity="1" d="M0,160L48,181.3C96,203,192,245,288,224C384,203,480,117,576,90.7C672,64,768,96,864,128C960,160,1056,192,1152,202.7C1248,213,1344,203,1392,197.3L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+      </svg>
+    </div>
+    <div id="section-2" class="vh-100 snap-2">
+
+    </div>
   </div>
 </template>
 
