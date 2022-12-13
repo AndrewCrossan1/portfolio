96575fe3e980d225fce2f6fecc1260a0dac105bd
AndrewCrossan1 <andrew.crossan23@outlook.com>
2022-12-13 16:25:09 +0000

    Add SearchBar component

---
+++ src/components/Forms/SearchBar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="SearchBar">
+        
+    </div>
+</template>
+
+<style>
+</style>
+
+<script>
+    export default {
+        name: "SearchBar",
+        data() {
+            return {
+                search: '',
+            }
+        },
+        methods: {
+            search() {
+                console.log("You searched for: " + this.search)
+            }
+        }
+    }
+</script>
