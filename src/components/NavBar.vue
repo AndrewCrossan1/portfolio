db2698f56d5bc6e11a5607a72375bf6c075b5d3a
AndrewCrossan1 <andrew.crossan23@outlook.com>
2022-12-13 18:45:04 +0000

    Added transparency to the navbar and modified colours and padding

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,9 +1,8 @@
 <template>
-    <div id="navbar" class="sticky-top">
-        <b-container fluid class="bg-graphite-gray">
-            <b-container size="md">
-                <b-navbar toggleable="md" class="p-2 sticky-top">
-                    <b-navbar-brand href="/" class="text-cornflower-blue">
+    <div id="navbar">
+        <b-container fluid>
+                <b-navbar toggleable="md" class="p-4 sticky-top px-5">
+                    <b-navbar-brand href="/" class="text-graphite-gray">
                         Andy's Portfolio
                     </b-navbar-brand>
                     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
@@ -11,7 +10,7 @@
                         <!--Right hand side of navbar-->
                         <b-navbar-nav class="ms-auto">
                             <b-nav-item>
-                                <b-link :to="{ name: 'home' }" :active="$route.name === 'home'" class="nav-link text-cornflower-blue" tag="a">
+                                <b-link :to="{ name: 'home' }" :active="$route.name === 'home'" class="nav-link" tag="a">
                                     <b-icon
                                         icon="house-door-fill"
                                     />
@@ -21,7 +20,6 @@
                         </b-navbar-nav>
                     </b-collapse>
                 </b-navbar>
-            </b-container>
         </b-container>
     </div>
 </template>
