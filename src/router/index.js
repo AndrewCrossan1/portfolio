cf1cdac48d3b3b1a1c594a14d627163e2ac32286
AndrewCrossan1 <andrew.crossan23@outlook.com>
2022-12-14 22:52:51 +0000

    Changed title of homepage

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,7 @@ const routes = [
     meta: {
       requiresAuth: false,
       seo: {
-        title: 'Home',
+        title: 'Home | Andrew Crossan',
         metaTags: [
           {
             name: 'description',
