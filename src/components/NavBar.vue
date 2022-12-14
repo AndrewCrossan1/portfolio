bca1f76429beb6604574aaa63828e2af024733bb
AndrewCrossan1 <andrew.crossan23@outlook.com>
2022-12-14 04:05:47 +0000

    Rearranged navbar menu, added search bar and made sticky

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,14 +1,11 @@
 <template>
-    <div id="navbar">
+    <div id="navbar" class="sticky-top">
         <b-container fluid>
-                <b-navbar toggleable="md" class="p-4 sticky-top px-5">
-                    <b-navbar-brand href="/" class="text-graphite-gray">
-                        Andy's Portfolio
-                    </b-navbar-brand>
+                <b-navbar toggleable="md" class="p-4 px-5">
                     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
                     <b-collapse id="nav-collapse" is-nav>
                         <!--Right hand side of navbar-->
-                        <b-navbar-nav class="ms-auto">
+                        <b-navbar-nav>
                             <b-nav-item>
                                 <b-link :to="{ name: 'home' }" :active="$route.name === 'home'" class="nav-link" tag="a">
                                     <b-icon
@@ -18,8 +15,37 @@
                                 </b-link>
                             </b-nav-item>
                         </b-navbar-nav>
+                        <b-navbar-brand class="mx-auto text-bebas-neue text-xl">
+                            Andrew Crossan Sites
+                        </b-navbar-brand>
+                        <b-form inline class="ml-auto">
+                            <div class="input-group">
+                                <input class="form-control search-form" type="text" id="example-search-input" v-model="searchData" placeholder="Search the site">
+                                <span class="input-group-append">
+                                    <button class="btn btn-graphite-gray rounded-0 ms-n3 search" @click="search()" type="button">
+                                        <b-icon icon="search"/>
+                                    </button>
+                                </span>
+                            </div>
+                        </b-form>
                     </b-collapse>
                 </b-navbar>
         </b-container>
     </div>
 </template>
+
+<script>
+ export default {
+    name: "NavBar",
+    data() {
+        return {
+            searchData: "",
+        };
+    },
+    methods: {
+        search() {
+            console.log(this.searchData);
+        },
+    },
+ }
+</script>
