d98ddc93e1c9f31ffeae2f6151993ded08961ccc
AndrewCrossan1 <andrew.crossan23@outlook.com>
2022-12-14 22:52:41 +0000

    Rejigged navbar, links centered and brand moved to left

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,11 +1,14 @@
 <template>
-    <div id="navbar" class="sticky-top">
+    <div id="navbar" class="sticky-top custom-nav">
         <b-container fluid>
-                <b-navbar toggleable="md" class="p-4 px-5">
+                <b-navbar toggleable="md" class="p-4 px-5 navbar-brand-center">
+                    <!--Right hand side of navbar-->
+                    <b-navbar-brand class="text-bebas-neue text-xl d-md-none d-lg-inline-block d-lg-block d-sm-block">
+                        <span class="navbar-logo">Andrew Crossan Sites</span>
+                    </b-navbar-brand>
                     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
                     <b-collapse id="nav-collapse" is-nav>
-                        <!--Right hand side of navbar-->
-                        <b-navbar-nav>
+                        <b-navbar-nav class="mx-auto"> 
                             <b-nav-item>
                                 <b-link :to="{ name: 'home' }" :active="$route.name === 'home'" class="nav-link" tag="a">
                                     <b-icon
@@ -15,9 +18,6 @@
                                 </b-link>
                             </b-nav-item>
                         </b-navbar-nav>
-                        <b-navbar-brand class="mx-auto text-bebas-neue text-xl">
-                            Andrew Crossan Sites
-                        </b-navbar-brand>
                         <b-form inline class="ml-auto">
                             <div class="input-group">
                                 <input class="form-control search-form" type="text" id="example-search-input" v-model="searchData" placeholder="Search the site">
